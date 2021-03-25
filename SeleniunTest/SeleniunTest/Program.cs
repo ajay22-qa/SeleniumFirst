e5c35ccef37cdad09a6f4afc34063dffc316337e
ajay22-qa <tech.ajaythakur@outlook.com>
2021-03-25 16:35:26 +0530

    2 Commit

--- SeleniunTest/SeleniunTest/Program.cs
+++ SeleniunTest/SeleniunTest/Program.cs
@@ -18,6 +18,7 @@ namespace SeleniunTest
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
             Application.Run(new Form1());
+            Application.Run(new Form1());
         }
     }
 }
