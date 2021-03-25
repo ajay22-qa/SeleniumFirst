7bad627abf097412fa7f065dc5cc203e03675fac
ajay22-qa <tech.ajaythakur@outlook.com>
2021-03-25 16:38:42 +0530

    2 Commit

--- SeleniunTest/SeleniunTest/Program.cs
+++ SeleniunTest/SeleniunTest/Program.cs
@@ -19,6 +19,7 @@ namespace SeleniunTest
             Application.Run(new Form1());
             Application.Run(new Form1());
             Application.Run(new Form1());
+            Application.Run(new Form1());
         }
     }
 }
